3b986f1bf8d54fd1531698e6c1f094c3b9c44d43
tikenov <azamatkz90@gmail.com>
2020-03-05 19:15:08 +0600

    Updates

---
+++ precache-manifest.20a46ed469d16559bd815a6433472e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f925308c79d30a258ee5736dfc8ab9a9",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "29a5a7dad728a0dd334a",
+    "url": "/simple-todo/static/css/main.83cc66bc.chunk.css"
+  },
+  {
+    "revision": "975daf232b0f323b5379",
+    "url": "/simple-todo/static/js/2.d4a63769.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/simple-todo/static/js/2.d4a63769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a5a7dad728a0dd334a",
+    "url": "/simple-todo/static/js/main.87a336cc.chunk.js"
+  },
+  {
+    "revision": "a9e5234f9fdba211f973",
+    "url": "/simple-todo/static/js/runtime-main.d3a2ba0d.js"
+  }
+]);
